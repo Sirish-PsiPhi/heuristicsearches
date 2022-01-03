a8581c1b599fae46eb2e7fc0e8656531f9388376
Sirish <b17sirish@gmail.com>
2022-01-03 18:14:06 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='heuristicsearch',
+    version='0.0.1',
+    description='Heuristic Searches',
+    py_modules=['a_star_search','ao_star','graph'],
+    package_dir={'':'src'}
+)
